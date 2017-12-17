7097527b70f7c3510119a76460176fcaf195846a
JayapradhaR <34539218+JayapradhaR@users.noreply.github.com>
2017-12-17 12:23:22 +0530

    Create Positive.c

---
+++ Positive.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num;
+scanf("%d",&num);
+if(num>0)
+{
+printf("Positive");
+}
+else if(num<0)
+{
+printf("Negative");
+}
+else 
+{
+printf("Zero");
+}
+}
